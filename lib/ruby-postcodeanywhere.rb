45026595a64a650c3e8eedbeeb5f33c940c576dd
Chris Norman <chris@norman.me>
2011-06-22 14:46:41 +0100

    Added fetch by postcode/building method

--- lib/ruby-postcodeanywhere.rb
+++ lib/ruby-postcodeanywhere.rb
@@ -4,6 +4,8 @@ module PostcodeAnywhere
   
   ADDRESS_LOOKUP = "http://services.postcodeanywhere.co.uk/xml.aspx"
   ADDRESS_FETCH = "http://services.postcodeanywhere.co.uk/dataset.aspx?action=fetch"
+  
+  RETRIEVE_BY_PARTS_URL = "http://services.postcodeanywhere.co.uk/PostcodeAnywhere/Interactive/RetrieveByParts/v1.00/xmla.ws?"
 
   # Account codes to access the PostcodeAnywhere Service
   mattr_accessor :account_code
@@ -11,8 +13,6 @@ module PostcodeAnywhere
   mattr_accessor :license_code
   @@license_code = "TEST"
 
-  # Default way to setup Devise. Run rails generate devise_install to create
-  # a fresh initializer with all configuration values.
   def self.setup
     yield self
   end
@@ -22,7 +22,7 @@ module PostcodeAnywhere
   	include HTTParty
   	format :xml
 
-  	attr_accessor :postcode, :country_code, :fetch_id
+  	attr_accessor :postcode, :country_code, :fetch_id, :building
 
     def initialize(args={})
       self.postcode = args[:postcode]
@@ -38,7 +38,24 @@ module PostcodeAnywhere
   		end
   		formatted_data
   	end
-	
+
+  	def fetch_by_parts
+  		data = PostcodeSearch.get self.fetch_by_parts_url
+  		formatted_data = data["NewDataSet"]["Data"]
+  		@address_lookup = AddressLookup.new
+      
+      if self.country_code == 'GB'
+        @address_lookup.postcode = formatted_data["postcode"]
+        @address_lookup.address_line_1 = formatted_data["line1"]
+        @address_lookup.address_line_2 = formatted_data["line2"]
+        @address_lookup.address_line_3 = formatted_data["line3"]
+        @address_lookup.post_town = formatted_data["post_town"]
+        @address_lookup.county = formatted_data["county"].blank? ? formatted_data["post_town"] : formatted_data["county"]
+      end
+      
+      @address_lookup
+  	end
+
   	def fetch
   		data = PostcodeSearch.get self.fetch_url
   		formatted_data = data["NewDataSet"]["Data"]
@@ -58,7 +75,7 @@ module PostcodeAnywhere
         @address_lookup.post_town = formatted_data["city"]
         @address_lookup.county = formatted_data["county_name"]+", "+formatted_data["state"]
       else
-          
+
       end
       @address_lookup
   	end
@@ -67,8 +84,8 @@ module PostcodeAnywhere
   		ADDRESS_LOOKUP+"?"+self.lookup_type+"&"+self.postcode_with_no_spaces+self.selected_country+"&"+self.license_information
   	end
 
-  	def fetch_url
-  		ADDRESS_FETCH+"&"+self.address_fetch_id+self.selected_country+"&"+self.license_information
+  	def fetch_by_parts_url
+  		RETRIEVE_BY_PARTS_URL+"&"+self.address_building+"&"+self.address_fetch_id+self.selected_country+"&"+self.license_information
   	end
 	
 	  def selected_country
@@ -81,6 +98,10 @@ module PostcodeAnywhere
 
   	def address_fetch_id
   		"id="+self.fetch_id
+  	end
+
+  	def address_building
+  		"building="+self.building
   	end
 
   	def lookup_type
