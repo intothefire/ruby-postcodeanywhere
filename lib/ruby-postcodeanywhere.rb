cda2970286b8eef3aafcda2a8bd9412111ec866e
Morgan Christiansson <morgan.christiansson@fundingcircle.com>
2012-02-21 11:32:12 +0000

    fix rspec

--- lib/ruby-postcodeanywhere.rb
+++ lib/ruby-postcodeanywhere.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'active_support/core_ext/module/attribute_accessors'
 
 module PostcodeAnywhere
 
--- spec/ruby-postcodeanywhere_spec.rb
+++ spec/ruby-postcodeanywhere_spec.rb
@@ -1,6 +1,6 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
-describe "RubyPostcodeanywhere" do
+describe PostcodeAnywhere do
   it "fails" do
     fail "hey buddy, you should probably rename this file and start specing for real"
   end
